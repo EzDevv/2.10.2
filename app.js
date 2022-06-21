17d6085b866afc934f13f774d6acdeb6590f6801
Evgeniy Antonov <zhekahead@gmail.com>
2022-06-21 16:17:54 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let mainQuestion = prompt("Какое <<Официальное>> название JavaScript?", "");
+
+if (mainQuestion == "ECMAScript") {
+    alert("Верно!")
+} else {
+    alert("Не знаете? <<ECMAScript>>");
+}
